1a61cfdf55fc236da8bcf7b3125857e1023e5a67
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-01-29 20:09:35 +0330

    Added: hyperlinks

--- mukette.c
+++ mukette.c
@@ -6,6 +6,15 @@
 
 #include <curses.h>
 
+#ifndef MAX_HYPERLINK
+#define MAX_HYPERLINK	4096
+#endif
+
+typedef struct Pair Pair;
+static struct Pair { 
+	int y, x; 
+	char *link; } hyperlinks[MAX_HYPERLINK] = {{0,0}};
+
 #define ATTR_ADDSTR(ATTR, TEXT)								\
 	do { attron(ATTR); addstr(TEXT); attroff(ATTR); } while (0)
 
@@ -21,13 +30,23 @@
 #define PRINT_BOLD_ITALIC_UNDERLINE(text)	ATTR_ADDSTR(A_BOLD | A_UNDERLINE | A_ITALIC, text)
 #define PRINT_COLOR_PAIR(pnum, text)		ATTR_ADDSTR(COLOR_PAIR(pnum), text)
 
+#define 
+
 #define NEWLINE()				addch('\n')
 #define WAIT()					getch();
 
 static inline void init_screen(void) { initscr(); }
 static inline void end_screen(void) { endwin(); }
-
-
+static inline Pair new_pair(int y, int x, char *link) { return (Pair){ 
+							.y = y, 
+							.x = x,
+							.link = link }; }
+static inline char *get_link(int y, int x) {
+	for (size_t i = 0; i < MAX_HYYPERLINK; i++)
+		if (hyperlinks[i].x == x && hyperlinks[i].y == y)
+			return hyperlinks[i].link;
+	return NULL;
+}
 
 int main(int argc, char **argv) {
    init_screen();
