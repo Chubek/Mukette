c9d7ed1cb7ccc931878168438165bb7f9e1cfc9d
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-02-01 02:15:27 +0330

    Re-purposed: the old file is now gone, replaced with the global declarations for Mukette: The Syntax-Variant Pager

---
+++ mukette.h
@@ -0,0 +1,17 @@
+#ifndef MUKETTE_H
+#define MUKETTE_H
+
+#include <curses.h>
+#include <spawn.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+typedef struct SymbolTable SymbolTable;
+typedef uint64_t hash_t;
+
+#endif /* mukette.h */
