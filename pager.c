ad69311f578ed1493cbc63da8b6022303b59c490
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-02-08 03:37:50 +0330

    Added: functions and additional fields to Naviable structure --- and it does not rely on Global Variables lazily

--- pager.c
+++ pager.c
@@ -15,10 +15,9 @@ typedef struct Navigatable {
   Naviable *next;
 } Naviable;
 
-static Naviable *navs = NULL;
-
-void insert_naviable(NaviableType type, int y, int x, int width, 
-		const char *contents, const char *tag, WINDOW *win) {
+void insert_naviable(Naviable **navs, NaviableType type, int y, int x,
+                     int width, const char *contents, const char *tag,
+                     WINDOW *win) {
   size_t contents_len = strlen(contents);
   Naviable *nav = (Naviable *)calloc(1, sizeof(Naviable));
   nav->type = type;
@@ -32,81 +31,77 @@ void insert_naviable(NaviableType type, int y, int x, int width,
   nav->next = navs;
   nav->prev = NULL;
   if (navs != NULL) {
-    navs->prev = nav;
+    (*navs)->prev = nav;
   }
-  navs = nav;
+  *navs = nav;
 }
 
-void free_naviable_list() {
+void free_naviable_list(Naviable *navs) {
   Naviable *current = navs;
   while (current != NULL) {
     Naviable *temp = current;
     current = current->next;
     if (temp->contents != NULL)
-	    free((void*)temp->contents);
+      free((void *)temp->contents);
     if (temp->tag != NULL)
-	    free((void*)temp->tag);
+      free((void *)temp->tag);
     if (temp->win != NULL)
-	    delwin(temp->win);
+      delwin(temp->win);
     free(temp);
   }
   navs = NULL;
 }
 
+void add_naviable_hyperlink(Naviable **navs, const char *name,
+                            const char *addr) {
+  GETPOS(y, x);
 
-void add_naviable_hyperlink(const char *name, const char *addr) {
-   GETPOS(y, x);
-   size_t name_len = strlen(name);
-   insert_naviable(HYPERLINK, y, x, name_len, addr, tag, NULL);
-   refresh();
+  size_t name_len = strlen(name);
+  insert_naviable(navs, HYPERLINK, y, x, name_len, addr, tag, NULL);
+
+  refresh();
 }
 
-void add_naviable_listing(const char *prog, const char **code_lines, size_t num_lines) {
-   GETPOS(y, x);
-   GETMAX(my, mx);
+void add_naviable_listing(Naviable **navs, const char *prog, const char *code) {
+  GETPOS(y, x);
 
-   WINDOW *win = newwin(num_lines, my / 3, y, mx / 3);
+  size_t code_len = strlen(code);
+  insert_naviable(navs, CODE_LISTING, y, x, code_len, code, prog, NULL);
 
-   while (--num_lines) {
-	waddstr(win, *code_lines);
-	insert_naviable(CODE_LISTING, y, x, strlen(*code_lines), *code_lines, prog, win);
-	code_lines++;
-	wrefresh(win);
-   }
+  refresh();
 }
 
-void naviable_execute(Naviable *nav, bool use_env) {
+void naviable_execute(Naviable *nav, bool use_env, bool exec_allowed) {
+  if (!exec_allowed) {
+    fprintf("Execution of external programs is prohibited unless you pass -x "
+            "to the program\n");
+    return;
+  }
+
   if (nav->type == HYPERLINK) {
-	 char *buffer, *open;
-	 if (__linux__)
-		open = "xdg-open";
-	 else if (__APPLE__)
-		open = "open";
-	 else 
-		 open = "chrome";
+    char *buffer, *open;
+    if (__linux__)
+      open = "xdg-open";
+    else if (__APPLE__)
+      open = "open";
+    else
+      open = "chrome";
 
-	 asprintf(buffer, "%s %s" open, nav->contents);
-	 system(buffer);
-	 free(buffer);
+    asprintf(buffer, "%s %s", open, nav->contents);
+    system(buffer);
+    free(buffer);
   } else {
-        char *buffer;
-        char tmpfile_name[] = { '/', 't', 'm', 'p', '/', 'X', 'X', 'X', '.', 'c', '\0' };
-	int fdesc = mkstemp(&tmpfile_name[0]);
-	FILE *fstream = fdopen(fdesc, "w");
-	fputs(fstream, nav->contents);
-	asprintf(buffer, "cat %s | %s %s",
-				&tmpfile_name[0],
-				use_env ? "env -S" : "",
-				nav->tag
-			);
-	system(buffer);
-	free(buffer);
-	fclose(fstream);
-	unlink(&tmpfile_name[0]);
-    }
-
+    char *buffer;
+    char tmpfile_name[] = {'/', 't', 'm', 'p', '/', 'X',
+                           'X', 'X', '.', 'c', '\0'};
+    int fdesc = mkstemp(&tmpfile_name[0]);
+    FILE *fstream = fdopen(fdesc, "w");
+    fputs(fstream, nav->contents);
+    asprintf(buffer, "cat %s | %s %s", &tmpfile_name[0],
+             use_env ? "env -S" : "", nav->tag);
+    system(buffer);
+    free(buffer);
+    fclose(fstream);
+    unlink(&tmpfile_name[0]);
+  }
 }
-
-
-
-
