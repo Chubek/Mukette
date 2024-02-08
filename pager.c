32171dd81e01aec0850fda96e12a17dab6497bde
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-02-08 07:37:11 +0330

    Fixed: compiles properly

--- pager.c
+++ pager.c
@@ -1,6 +1,6 @@
 #include "pager.h"
 
-typedef enum NavigatbleType {
+typedef enum NavigatableType {
   HYPERLINK,
   CODE_LISTING,
 } NaviableType;
@@ -28,7 +28,7 @@ void insert_naviable(Naviable **navs, NaviableType type, int y, int x,
   nav->tag = tag;
   nav->win = win;
 
-  nav->next = navs;
+  nav->next = *navs;
   nav->prev = NULL;
   if (navs != NULL) {
     (*navs)->prev = nav;
@@ -52,12 +52,34 @@ void free_naviable_list(Naviable *navs) {
   navs = NULL;
 }
 
+static inline void highlight_naviable(Naviable *nav) {
+  mvchgat(nav->y, nav->x, nav->width, A_STANDOUT, COLOR_PAIR(0), NULL);
+}
+
+static inline void unhighlight_naviable(Naviable *nav) {
+  mvchgat(nav->y, nav->x, nav->width, A_NORMAL, COLOR_PAIR(0), NULL);
+}
+
+static inline bool next_naviable(Naviable *nav) {
+  if (nav->next == NULL)
+    return false;
+  nav = nav->next;
+  return true;
+}
+
+static inline bool previous_naviable(Naviable *nav) {
+  if (nav->prev == NULL)
+    return false;
+  nav = nav->prev;
+  return true;
+}
+
 void add_naviable_hyperlink(Naviable **navs, const char *name,
                             const char *addr) {
   GETPOS(y, x);
 
   size_t name_len = strlen(name);
-  insert_naviable(navs, HYPERLINK, y, x, name_len, addr, tag, NULL);
+  insert_naviable(navs, HYPERLINK, y, x, name_len, name, addr, NULL);
 
   refresh();
 }
@@ -73,21 +95,22 @@ void add_naviable_listing(Naviable **navs, const char *prog, const char *code) {
 
 void naviable_execute(Naviable *nav, bool use_env, bool exec_allowed) {
   if (!exec_allowed) {
-    fprintf("Execution of external programs is prohibited unless you pass -x "
+    fprintf(stderr, "Warning: Execution of external programs is prohibited unless you pass -x "
             "to the program\n");
     return;
   }
 
   if (nav->type == HYPERLINK) {
     char *buffer, *open;
-    if (__linux__)
-      open = "xdg-open";
-    else if (__APPLE__)
-      open = "open";
-    else
-      open = "chrome";
+#ifdef __linux__
+    open = "xdg-open";
+#elif __APPLE__
+    open = "open";
+#else
+    open = "chrome";
+#endif
 
-    asprintf(buffer, "%s %s", open, nav->contents);
+    asprintf(&buffer, "%s %s", open, nav->contents);
     system(buffer);
     free(buffer);
   } else {
@@ -96,8 +119,8 @@ void naviable_execute(Naviable *nav, bool use_env, bool exec_allowed) {
                            'X', 'X', '.', 'c', '\0'};
     int fdesc = mkstemp(&tmpfile_name[0]);
     FILE *fstream = fdopen(fdesc, "w");
-    fputs(fstream, nav->contents);
-    asprintf(buffer, "cat %s | %s %s", &tmpfile_name[0],
+    fputs(nav->contents, fstream);
+    asprintf(&buffer, "cat %s | %s %s", &tmpfile_name[0],
              use_env ? "env -S" : "", nav->tag);
     system(buffer);
     free(buffer);
@@ -110,7 +133,7 @@ void poll_and_navigate(Naviable **navs, bool use_env, bool exec_allowed) {
   int c = 0;
   Naviable *current_nav = *navs;
 
-  while (;;) {
+  for (;;) {
     c = getch();
     highlight_naviable(current_nav);
     switch (c) {
@@ -118,20 +141,25 @@ void poll_and_navigate(Naviable **navs, bool use_env, bool exec_allowed) {
     case KEY_NEXT:
     case 'n':
     case 'N':
-      unhighligh_navible(current_nav);
+      unhighlight_naviable(current_nav);
       if (!next_naviable(current_nav))
         current_nav = *navs;
       break;
     case KEY_LEFT:
-    case KEY_PREV:
+    case KEY_PREVIOUS:
     case 'p':
-    case 'N':
+    case 'P':
       unhighlight_naviable(current_nav);
       if (!previous_naviable(current_nav))
         current_nav = *navs;
       break;
+    case KEY_STAB:
+    case '\t':
+      unhighlight_naviable(current_nav);
+      if (!next_naviable(current_nav))
+        current_nav = *navs;
+      break;
     case KEY_ENTER:
-    case KEY_SPACE:
     case '\r':
     case ' ':
       naviable_execute(current_nav, use_env, exec_allowed);
